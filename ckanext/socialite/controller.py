734d98cc7b8dcdd1c1a72bfefe700c41d5699ab4
ccancellieri <carlo.cancellieri@fao.org>
2020-07-12 18:57:00 +0200

    login works

---
+++ ckanext/socialite/controller.py
@@ -0,0 +1,112 @@
+'''
+Repoze.who Firebase controller
+'''
+
+import logging
+import re
+
+from pylons.i18n import _
+
+import ckan.controllers.user as user
+import ckan.lib.base as base
+from ckan.common import g, request, response, config, session
+import ckan.plugins.toolkit as toolkit
+import json
+import uuid
+
+log = logging.getLogger(__name__)
+
+
+class UserFirebaseController(user.UserController):
+
+    def login(self):
+        """Login the user with credentials from the SocialAuth used. The CKAN
+        username is created and access given.
+        """
+
+        login_path = base.config.get("ckanext.firebase.login_path", "/firebase/login")
+
+        locale = request.environ.get('CKAN_LANG')
+        login_path = re.sub('{{LANG}}', str(locale), login_path)
+
+        # toolkit.request.environ
+        # https://github.com/eHealthAfrica/ckanext-oauth2/blob/master/ckanext/oauth2/oauth2.py
+
+        params = json.loads(request.body) #toolkit.request.params
+        if 'uid' in params:
+            user_account = params['email'].split('@')[0]
+            full_name = params['displayName']
+            user_email = params['email']
+            if user_account.isalnum() is False:
+                user_account = ''.join(e for e in user_account if e.isalnum())
+
+            user_ckan = self.get_ckanuser(user_account)
+
+            if not user_ckan:
+                user_ckan = toolkit.get_action('user_create')(
+                    context={'ignore_auth': True},
+                    data_dict={'email': user_email,
+                               'name': user_account,
+                               'fullname': full_name,
+                               'password': self.get_ckanpasswd()})
+
+            session['ckanext_user'] = user_ckan['name']
+            session['ckanext_email'] = user_email
+            session.save()
+
+        #return base.h.redirect_to(login_path)
+
+        # if base.c.userobj is not None:
+        #     log.info("Repoze.who Shibboleth controller received userobj %r " % base.c.userobj)
+        #     return base.h.redirect_to(controller='user',
+        #                               action='read',
+        #                               id=base.c.userobj.name)
+        # else:
+        #     log.error("No userobj received in Repoze.who Shibboleth controller %r " % base.c)
+        #     base.h.flash_error(_("No user info received for login"))
+        #     return base.h.redirect_to('/')
+
+    def get_ckanuser(self, user):
+        """Return CKAN user if it already exists."""
+        import ckan.model
+
+        user_ckan = ckan.model.User.by_name(user)
+
+        if user_ckan:
+            user_dict = toolkit.get_action('user_show')(data_dict={'id': user_ckan.id})
+            return user_dict
+        else:
+            return None
+
+    def get_ckanpasswd(self):
+        """Generate strong password for CKAN user."""
+        import datetime
+        import random
+        passwd = str(random.random()) + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") + str(uuid.uuid4().hex)
+        passwd = re.sub(r"\s+", "", passwd, flags=re.UNICODE)
+        return passwd
+
+    def _logout_user(self):
+        """Log out the currently logged in CKAN user."""
+        # import pylons
+        # to revoke the Google token uncomment the code below
+        # if 'ckanext_-accesstoken' in session:
+        #    atoken = session.get('ckanext_-accesstoken')
+        #    res = requests.get('https://accounts.google.com/o/oauth2/revoke?token='+atoken)
+        #    if res.status_code == 200:
+        #       del session['ckanext_-accesstoken']
+        #    else:
+        #   raise GoogleAuthException('Token not revoked')
+        if 'ckanext_user' in session:
+            del session['ckanext_user']
+        if 'ckanext_email' in session:
+            del session['ckanext_email']
+        session.save()
+
+    def logout(self):
+
+        logout_path = base.config.get("ckanext.firebase.logout_path", "/firebase/logout")
+        self._logout_user(self);
+        #return base.h.redirect_to(logout_path)
+
+
--- ckanext/socialite/plugin.py
+++ ckanext/socialite/plugin.py
@@ -26,7 +26,6 @@ SOFTWARE.
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import json
-import uuid
 import ckan.lib.helpers as helpers
 import requests
 import re
@@ -58,6 +57,7 @@ class SocialitePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IAuthenticator)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     def update_config(self, config_):
         """Add resources used by the plugin into core config file."""
@@ -65,76 +65,30 @@ class SocialitePlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'googleauth')
 
+    def before_map(self, map):
+        """
+        Override IRoutes.before_map()
+        """
+        controller = 'ckanext.socialite.controller:UserFirebaseController'
+        map.connect('firebaseLogin',
+                    "/firebase/login",
+                    controller=controller,
+                    action='login')
+        map.connect('firebaseLogout',
+                    "/firebase/logout",
+                    controller=controller,
+                    action='logout')
+        return map
+
     def get_helpers(self):
         """Declare new helper functions."""
         return {'googleauth_get_clientid': get_google_clientid,
                 'googleauth_get_hosted_domain': get_hosted_domain}
 
-    def get_ckanuser(self, user):
-        """Return CKAN user if it already exists."""
-        import ckan.model
-
-        user_ckan = ckan.model.User.by_name(user)
-
-        if user_ckan:
-            user_dict = toolkit.get_action('user_show')(data_dict={'id': user_ckan.id})
-            return user_dict
-        else:
-            return None
-
-    def get_ckanpasswd(self):
-        """Generate strong password for CKAN user."""
-        import datetime
-        import random
-        passwd = str(random.random()) + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")+str(uuid.uuid4().hex)
-        passwd = re.sub(r"\s+", "", passwd, flags=re.UNICODE)
-        return passwd
-
-    def _logout_user(self):
-        """Log out the currently logged in CKAN user."""
-        # import pylons
-        # to revoke the Google token uncomment the code below
-        # if 'ckanext_-accesstoken' in session:
-        #    atoken = session.get('ckanext_-accesstoken')
-        #    res = requests.get('https://accounts.google.com/o/oauth2/revoke?token='+atoken)
-        #    if res.status_code == 200:
-        #       del session['ckanext_-accesstoken']
-        #    else:
-        #   raise GoogleAuthException('Token not revoked')
-        if 'ckanext_user' in session:
-            del session['ckanext_user']
-        if 'ckanext_email' in session:
-            del session['ckanext_email']
-        session.save()
 
     def login(self):
-        """Login the user with credentials from the SocialAuth used. The CKAN
-        username is created and access given.
-        """
-        # toolkit.request.environ
-        # https://github.com/eHealthAfrica/ckanext-oauth2/blob/master/ckanext/oauth2/oauth2.py
+       pass
 
-        params = toolkit.request.params
-        if 'id_token' in params:
-            user_account = params['email'].split('@')[0]
-            full_name = params['name']
-            user_email = params['email']
-            if user_account.isalnum() is False:
-                user_account = ''.join(e for e in user_account if e.isalnum())
-
-            user_ckan = self.get_ckanuser(user_account)
-
-            if not user_ckan:
-                user_ckan = toolkit.get_action('user_create')(
-                                        context={'ignore_auth': True},
-                                        data_dict={'email': user_email,
-                                                   'name': user_account,
-                                                   'fullname': full_name,
-                                                   'password': self.get_ckanpasswd()})
-
-            session['ckanext_user'] = user_ckan['name']
-            session['ckanext_email'] = user_email
-            session.save()
 
     # if someone is logged in will be set the parameter c.user
     def identify(self):
